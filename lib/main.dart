22d9c10ed6576fe11969e4d205b2ddff8719c20e
BrushankPatra <72485994+BrushankPatra@users.noreply.github.com>
2023-05-02 22:20:15 +0530

    added colors and gradient

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,21 @@ void main() {
     MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Roll Dice"),
+          backgroundColor: Colors.pink[300],
+          title: const Text("Roll Dice"),
         ),
-        body: Center(
-          child: Text("Hello World!"),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 255, 133, 174),
+                Colors.white,
+              ],
+            ),
+          ),
+          child: const Center(
+            child: Text("Hello World!"),
+          ),
         ),
       ),
     ),
